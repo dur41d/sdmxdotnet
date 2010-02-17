d06c906256f0234cd6b3d8353cbae2174fe0f8da
duraid <duraid@localhost>
2010-02-17 17:25:15 -0500

    compact finsished 2

---
+++ src/SDMX/Maps/DataMaps/Compact/CompactDataSetMap.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OXM;
+using System.Xml.Linq;
+
+namespace SDMX.Parsers
+{
+    internal class CompactDataSetMap : ClassMap<DataSet>
+    {
+        DataSet _dataSet;
+        ValueConverter converter = new ValueConverter();
+
+        public CompactDataSetMap(KeyFamily keyFamily)
+        {
+            _dataSet = new DataSet(keyFamily);
+
+            MapCollection(o => o.Series).ToElement("Series", false)
+                .Set(v => _dataSet.Series.Add(v))
+                .ClassMap(() => new CompactSeriesMap(_dataSet));
+
+            string startTime = null;
+
+            foreach (var attribute in keyFamily.Attributes.Where(a => a.AttachementLevel == AttachmentLevel.DataSet))
+            {
+                var _att = attribute;
+                bool required = attribute.AssignmentStatus == AssignmentStatus.Mandatory;
+                Map(o => converter.Serialize((IValue)o.Attributes[_att.Concept.ID], out startTime)).ToAttribute(_att.Concept.ID.ToString(), required)
+                    .Set(v => _dataSet.Attributes[_att.Concept.ID] = converter.Parse(_att, v, null))
+                    .Converter(new StringConverter());
+            }
+        }
+
+        protected override DataSet Return()
+        {
+            return _dataSet;
+        }
+    }
+}
---
+++ src/SDMX/Maps/DataMaps/Compact/CompactSeriesKeyMap.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OXM;
+using System.Xml.Linq;
+
+namespace SDMX.Parsers
+{
+    public class CompactSeriesKeyMap : AttributeGroupTypeMap<ReadOnlyKey>
+    {
+        Key _key;
+        ValueConverter converter = new ValueConverter();
+
+        public CompactSeriesKeyMap(DataSet dataSet)
+        {
+            _key = dataSet.NewKey();
+            string startTime = null;
+
+            foreach (var dim in dataSet.KeyFamily.Dimensions)
+            {
+                var anchor = dim;
+                MapAttribute(o => converter.Serialize(o[anchor.Concept.ID], out startTime), anchor.Concept.ID.ToString(), true)
+                    .Set(v => _key[anchor.Concept.ID] = converter.Parse(anchor, v, null))
+                    .Converter(new StringConverter());
+            }
+        }
+
+        protected override ReadOnlyKey Return()
+        {
+            return new ReadOnlyKey(_key);
+        }
+    }
+}
