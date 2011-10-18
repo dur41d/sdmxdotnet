bcdb53309d46714a66ff1cc878a2bfbfefa23c96
duraid <duraid@localhost>
2011-10-18 10:32:16 -0400

    Add tests to SimpleTypeConverter.cs to cover conversion from null.

---
+++ src/OXM.Tests/StringConverterTests.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace OXM.Tests
+{
+
+    [TestFixture]
+    public class StringConverterTests
+    {
+        [Test]
+        public void StringConverter()
+        {
+            var converter = new StringConverter();
+
+            string value = "2001";
+            var obj = converter.ToObj(value);
+            Assert.AreEqual(value, converter.ToXml(obj));
+
+            value = null;
+            obj = converter.ToObj(value);
+            Assert.AreEqual(value, converter.ToXml(obj));
+        }
+
+        [Test]
+        public void StringConverter_object()
+        {
+            ISimpleTypeConverter converter = new StringConverter();
+
+            string value = "2001";
+            var obj = converter.ToObj(value);
+            Assert.AreEqual(value, converter.ToXml(obj));
+
+            value = null;
+            obj = converter.ToObj(value);
+            Assert.AreEqual(value, converter.ToXml(obj));
+        }
+    }
+}
--- src/SDMX/Structure/TextFormats/TimePeriodTextFormat.cs
+++ src/SDMX/Structure/TextFormats/TimePeriodTextFormat.cs
@@ -26,56 +26,5 @@ namespace SDMX
         {
             return typeof(TimePeriod);
         }
-
-        //public override object Parse(string s, string startTime)
-        //{
-        //    var value = _converter.Parse(s, startTime);
-
-        //    if (value is DateTimeValue) return (DateTimeOffset)(DateTimeValue)value;
-        //    else if (value is DateValue) return (DateTimeOffset)(DateValue)value;
-        //    else if (value is YearMonthValue) return (DateTimeOffset)(YearMonthValue)value;
-        //    else if (value is YearValue) return (DateTimeOffset)(YearValue)value;
-        //    else if (value is WeeklyValue
-        //        || value is QuarterlyValue
-        //        || value is BiannualValue
-        //        || value is TriannualValue)
-        //    {
-        //        return value.ToString();
-        //    }
-        //    else
-        //    {
-        //        throw new SDMXException("Cannot parse string: {0} star.", s);
-        //    }
-
-        //}
-
-        //public override string Serialize(object obj, out string startTime)
-        //{
-        //    Contract.AssertNotNull(obj, "obj");
-
-        //    if (obj is DateTime?)
-        //        obj = ((DateTime?)obj).Value;
-            
-        //    if (obj is DateTime)
-        //    { 
-        //        var dt = (DateTime)obj;
-        //        obj = new DateTimeOffset(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, dt.Millisecond, TimeSpan.FromTicks(0));
-        //    }
-
-        //    if (obj is DateTimeOffset?)
-        //        obj = ((DateTimeOffset?)obj).Value;
-            
-        //    if (obj is DateTimeOffset)
-        //    {
-        //        var dt = (DateTimeOffset)obj;
-
-        //        if (dt.Hour > 0)
-        //            obj = new DateTimeValue(dt);
-        //        else
-        //            obj = new DateValue(dt);
-        //    } 
-
-        //    return _converter.Serialize(obj, out startTime);
-        //}
     }
 }
