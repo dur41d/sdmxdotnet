df38f6be87a342de10e5d0ae18967287cff32911
duraid <duraid@localhost>
2012-01-06 16:23:33 -0500

    Expose the XmlReader, LineNumber, LinePosition properties. Add documentation for DataReader.cs. Add comments for SetGroup method.

--- src/SDMX/Data/GenerictDataReader.cs
+++ src/SDMX/Data/GenerictDataReader.cs
@@ -14,22 +14,22 @@ namespace SDMX
         {
             CheckDisposed();
 
-            while (_xmlReader.Read())
+            while (XmlReader.Read())
             {
-                if (_xmlReader.LocalName == "Group" && _xmlReader.IsStartElement())
+                if (XmlReader.LocalName == "Group" && XmlReader.IsStartElement())
                 {
-                    string groupId = _xmlReader.GetAttribute("type");
+                    string groupId = XmlReader.GetAttribute("type");
 
                     if (groupId == null)
-                        SDMXException.ThrowParseError(_xmlReader, "Group is missing type attribute.");
+                        SDMXException.ThrowParseError(XmlReader, "Group is missing type attribute.");
 
                     var group = KeyFamily.Groups.Find(groupId);
 
                     if (group == null)
-                        SDMXException.ThrowParseError(_xmlReader, "Keyfamily does not contain group with id: {0}.", groupId);
+                        SDMXException.ThrowParseError(XmlReader, "Keyfamily does not contain group with id: {0}.", groupId);
 
                     var dict = new Dictionary<string, object>();
-                    while (_xmlReader.Read() && _xmlReader.LocalName != "Series")
+                    while (XmlReader.Read() && XmlReader.LocalName != "Series")
                     {
                         if (IsValueElement())
                             ParseValue((n, v) => dict.Add(n, v));
@@ -39,11 +39,11 @@ namespace SDMX
                 } 
 
 
-                if (_xmlReader.LocalName == "Series" && _xmlReader.IsStartElement())
+                if (XmlReader.LocalName == "Series" && XmlReader.IsStartElement())
                 {
                     ClearSeries();
 
-                    while (_xmlReader.Read() && _xmlReader.LocalName != "Obs")
+                    while (XmlReader.Read() && XmlReader.LocalName != "Obs")
                     {
                         if (IsValueElement())
                         {
@@ -52,22 +52,22 @@ namespace SDMX
                     }
                 }
 
-                if (_xmlReader.LocalName == "Obs" && _xmlReader.IsStartElement())
+                if (XmlReader.LocalName == "Obs" && XmlReader.IsStartElement())
                 {                    
                     ClearObs();
 
-                    while (_xmlReader.Read() && !(_xmlReader.LocalName == "Obs" && !_xmlReader.IsStartElement()))
+                    while (XmlReader.Read() && !(XmlReader.LocalName == "Obs" && !XmlReader.IsStartElement()))
                     {
-                        if (_xmlReader.LocalName == "Time" && _xmlReader.IsStartElement())
+                        if (XmlReader.LocalName == "Time" && XmlReader.IsStartElement())
                         {
-                            string value = _xmlReader.ReadString();
+                            string value = XmlReader.ReadString();
                             // TODO: improve exception message to include line number by using Try parse
                             // move parsing to DataReader class
                             SetObs(KeyFamily.TimeDimension.Concept.Id.ToString(), KeyFamily.TimeDimension.Parse(value, null));
                         }
-                        else if (_xmlReader.LocalName == "ObsValue" && _xmlReader.IsStartElement())
+                        else if (XmlReader.LocalName == "ObsValue" && XmlReader.IsStartElement())
                         {
-                            string value = _xmlReader.GetAttribute("value");
+                            string value = XmlReader.GetAttribute("value");
                             SetObs(KeyFamily.PrimaryMeasure.Concept.Id.ToString(), KeyFamily.PrimaryMeasure.Parse(value, null));
                         }
                         else if (IsValueElement())
@@ -88,14 +88,14 @@ namespace SDMX
 
         private bool IsValueElement()
         {
-            return _xmlReader.LocalName == "Value" && _xmlReader.IsStartElement();
+            return XmlReader.LocalName == "Value" && XmlReader.IsStartElement();
         }
 
         void ParseValue(Action<string, object> set)
         {
-            string concept = _xmlReader.GetAttribute("concept");
-            string value = _xmlReader.GetAttribute("value");
-            string startTime = _xmlReader.GetAttribute("startTime");
+            string concept = XmlReader.GetAttribute("concept");
+            string value = XmlReader.GetAttribute("value");
+            string startTime = XmlReader.GetAttribute("startTime");
             var component = KeyFamily.GetComponent(concept);
             set(concept, component.Parse(value, startTime));
         }
